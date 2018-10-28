5c13aa08af34999b17205fc538d4aea6266e8bdb
Damien Thome Lutz <damien.lutz@operacao.rcadigital.com.br>
2018-10-28 13:42:40 -0200

    Experimentando log

--- src/main/java/pocs/swarm/api/BookApi.java
+++ src/main/java/pocs/swarm/api/BookApi.java
@@ -1,6 +1,8 @@
 package pocs.swarm.api;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pocs.swarm.api.dto.BookDto;
 import pocs.swarm.ejb.BookBO;
 
@@ -20,10 +22,14 @@ public class BookApi {
     private BookBO bookBO;
     private ModelMapper modelMapper = new ModelMapper();
 
+    Logger logger = LoggerFactory.getLogger(BookApi.class);
+
     @Path("{id:\\d+}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public BookDto buscaPorId(@PathParam("id") Long id){
+        logger.info("Recebida requisicao id: {}", id);
+
         return modelMapper.map( bookBO.findBook(id), BookDto.class);
     }
 }
